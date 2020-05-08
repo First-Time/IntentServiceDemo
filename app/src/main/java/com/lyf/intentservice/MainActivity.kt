4508a8be2e5434c26921f6bce74f8de9b9408f9b
liuyanfei <liuyanfei@jinjiedao.com>
2020-05-08 15:38:44 +0800

    [feature]代码

--- app/src/main/java/com/lyf/intentservice/MainActivity.kt
+++ app/src/main/java/com/lyf/intentservice/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.lyf.intentservice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        btn_start_foo.setOnClickListener {
+            MyIntentService.startActionFoo(this, "foo1", "foo1-1")
+        }
+
+        btn_start_baz.setOnClickListener {
+            MyIntentService.startActionBaz(this, "baz1", "baz1-1")
+        }
+
+        btn_start_foo_3.setOnClickListener {
+            MyIntentService.startActionFoo(this, "foo1", "foo1-1")
+            MyIntentService.startActionFoo(this, "foo2", "foo2-1")
+            MyIntentService.startActionFoo(this, "foo3", "foo3-1")
+        }
+
+        btn_start_baz_3.setOnClickListener {
+            MyIntentService.startActionBaz(this, "baz1", "baz1-1")
+            MyIntentService.startActionBaz(this, "baz2", "baz2-1")
+            MyIntentService.startActionBaz(this, "baz3", "baz3-1")
+
+        }
+
+        btn_start_foo_baz_3.setOnClickListener {
+            MyIntentService.startActionFoo(this, "foo1", "foo1-1")
+            MyIntentService.startActionBaz(this, "baz1", "baz1-1")
+            MyIntentService.startActionFoo(this, "foo2", "foo2-1")
+            MyIntentService.startActionBaz(this, "baz2", "baz2-1")
+            MyIntentService.startActionFoo(this, "foo3", "foo3-1")
+            MyIntentService.startActionBaz(this, "baz3", "baz3-1")
+        }
     }
 }
